a69f5c8960c26f67e598c3050680c4802f49a50c
Tonnyderrick <tonnyaketch@gmail.com>
2025-07-16 15:46:28 +0300

    Task 0: Basic IP Logging Middleware

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,25 @@
+# ip_tracking/middleware.py
+
+from .models import RequestLog
+from django.utils.timezone import now
+
+class IPLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip_address = self.get_client_ip(request)
+        path = request.path
+
+        RequestLog.objects.create(ip_address=ip_address, path=path, timestamp=now())
+
+        response = self.get_response(request)
+        return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
